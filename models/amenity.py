6ff4864df6bb180310a05e57fdc5ee1dab166fc6
Jessica Bathel <811@holbertonschool.com>
2019-12-19 16:45:07 +0000

    Fixed PEP8 error in console.py and updated models to us tables

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from slqalchemy import Column, String
+from slqalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
@@ -8,4 +10,5 @@ class Amenity(BaseModel):
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
