b08b26f805f033aeb8110dc6a21b8bb7dcbb346c
Banu Sapakova <banuaksom@gmail.com>
2019-12-19 01:38:29 +0000

    added relationship

--- models/state.py
+++ models/state.py
@@ -13,3 +13,5 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
