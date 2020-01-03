52887d6ef7805891584d345b3f44ee4bfb9990e6
Banu Sapakova <banuaksom@gmail.com>
2020-01-03 23:00:19 +0000

    edge case for the ignoring existed folder

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,10 +3,11 @@ from datetime import datetime
 from os.path import isfile
 from fabric.api import *
 
+
 def do_pack():
     """ generates a .tgz archive from the contents of the web_static """
     cur_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    local("mkdir versions")
+    local("mkdir -p versions")
     local("tar cvzf versions/web_static_{}.tgz web_static".format(cur_time))
     if isfile("versions/web_static_{}.tgz".format(cur_time)):
         return "versions/web_static_{}.tgz".format(cur_time)
