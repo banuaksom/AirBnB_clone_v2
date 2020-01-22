cd1a7decd0d33e70a8297c638ed4807767fad58c
Banuaksom <banuaksom@gmail.com>
2020-01-22 18:14:17 +0000

    first commit

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -12,5 +13,17 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """ returns the list of City objs from storage
+            linked to current state
+            """
+            cities = []
+            for city in models.storage.all(City).values():
+                if self.id == city.state_id:
+                    cities.append(city)
+            return cities
